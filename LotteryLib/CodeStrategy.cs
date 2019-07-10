562f42dc1503d1f9fea8b53a270e3d9b85715fd5
atLenluo <718723112@qq.com>
2019-07-10 15:00:04 +0800

    LenHotWenRandomHalf 优化

--- LotteryLib/CodeStrategy.cs
+++ LotteryLib/CodeStrategy.cs
@@ -1,15 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LotteryLib {
     /// <summary>
     /// 出号策略
     /// </summary>
     public class CodeStrategy {
-
         private Random random = new Random(new Guid().GetHashCode());
 
         /// <summary>
@@ -19,37 +15,42 @@ namespace LotteryLib {
         /// <param name="pos">位置</param>
         /// <param name="count">统计期数</param>
         /// <returns></returns>
-        public List<string> LenHotWenRandomHalf(IList<CodeData>list, int pos, int count) {
+        public List<string> LenHotWenRandomHalf(IList<CodeData> list, int pos, int count) {
             var hotCode = CodeUtil.GetHotCode(list, pos, count);
             var wenCode = CodeUtil.GetWenCode(list, pos, count);
             var lenCode = CodeUtil.GetLenCode(list, pos, count);
 
-            var hotNum = (int)Math.Ceiling(hotCode.Count / 2.0f);
-            var wenNum = (int)Math.Ceiling(wenCode.Count / 2.0f);
-            var lenNum = (int)Math.Ceiling(lenCode.Count / 2.0f);
+            var hotNum = (int) Math.Ceiling(hotCode.Count / 2.0f);
+            var wenNum = (int) Math.Ceiling(wenCode.Count / 2.0f);
+            var lenNum = (int) Math.Ceiling(lenCode.Count / 2.0f);
+
+            var hotGetNum = 0;
+            var wenGetNum = 0;
+            var lenGetNum = 0;
 
             var retCode = new List<string>();
-            int index = 0;
-            for (var i = 0; i < hotNum; i++) {
-                index = random.Next(hotCode.Count);
-                retCode.Add(hotCode[index]);
-                hotCode.RemoveAt(index);
-            }
+            while (retCode.Count < 5) {
+                var index = 0;
+                if (hotGetNum < hotNum) {
+                    index = random.Next(hotCode.Count);
+                    retCode.Add(hotCode[index]);
+                    hotCode.RemoveAt(index);
+                    hotGetNum++;
+                }
 
-            for (var i = 0; i < wenNum; i++) {
-                index = random.Next(wenCode.Count);
-                retCode.Add(wenCode[index]);
-                wenCode.RemoveAt(index);
-            }
+                if (wenGetNum < wenNum && retCode.Count < 5) {
+                    index = random.Next(wenCode.Count);
+                    retCode.Add(wenCode[index]);
+                    wenCode.RemoveAt(index);
+                    wenGetNum++;
+                }
 
-            for (var i = 0; i < lenNum; i++) {
-                index = random.Next(lenCode.Count);
-                retCode.Add(lenCode[index]);
-                lenCode.RemoveAt(index);
-            }
-
-            while (retCode.Count > 5) {
-                retCode.RemoveAt(retCode.Count - 1);
+                if (lenGetNum < lenNum && retCode.Count < 5) {
+                    index = random.Next(lenCode.Count);
+                    retCode.Add(lenCode[index]);
+                    lenCode.RemoveAt(index);
+                    lenGetNum++;
+                }
             }
 
             return retCode;
@@ -61,8 +62,7 @@ namespace LotteryLib {
         /// <param name="list">历史记录</param>
         /// <param name="pos">位置</param>
         /// <returns></returns>
-        public List<string> LeftMidRightRandomHalf(IList<CodeData>list, int pos)
-        {
+        public List<string> LeftMidRightRandomHalf(IList<CodeData> list, int pos) {
             var retCode = new List<string>();
             var midCode = list[0].codes[pos].ToString();
             retCode.Add(midCode);
@@ -70,14 +70,12 @@ namespace LotteryLib {
             var mid = Convert.ToInt32(midCode);
 
             var leftCode = new List<string>();
-            for (int i = 0; i < mid; i++)
-            {
+            for (int i = 0; i < mid; i++) {
                 leftCode.Add(i.ToString());
             }
 
             var rightCode = new List<string>();
-            for (int i = mid + 1; i < 10; i++)
-            {
+            for (int i = mid + 1; i < 10; i++) {
                 rightCode.Add(i.ToString());
             }
 
@@ -86,18 +84,15 @@ namespace LotteryLib {
             int leftGetNum = 0;
             int rightGetNum = 0;
             int index = 0;
-            while (retCode.Count < 5)
-            {
-                if (leftGetNum < leftNum)
-                {
+            while (retCode.Count < 5) {
+                if (leftGetNum < leftNum) {
                     index = random.Next(leftCode.Count);
                     retCode.Add(leftCode[index]);
                     leftCode.RemoveAt(index);
                     leftGetNum++;
                 }
 
-                if (rightGetNum < rightNum && retCode.Count < 5)
-                {
+                if (rightGetNum < rightNum && retCode.Count < 5) {
                     index = random.Next(rightCode.Count);
                     retCode.Add(rightCode[index]);
                     rightCode.RemoveAt(index);
